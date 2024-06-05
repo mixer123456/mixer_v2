0c75d074020bdbdda1388ad3612fd4f24a21a6b3
max <maxiepishkin@gmail.com>
2024-06-05 11:16:22 +0100

    code fixed

--- homework_07/homework.py
+++ homework_07/homework.py
@@ -1,9 +1,9 @@
 def welcome_user_message(username):
-    normalized_username = username.title().strip()
+    normalized_username = username.strip()
     return f'<h1>Вітаємо тебе, шановний {normalized_username}</h1>'
 
 
-def area_of_the_rectangle(a, b):
-    if a < 0 or b < 0:
-        return False
-    return a * b
+def calculating_area_rectangle(width, height):
+    # if width < 0 or height < 0:
+    #     return False
+    return width * height
