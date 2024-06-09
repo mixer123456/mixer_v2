484d1bbc0edef840799a5eebcbf42a3d10b6acae
max <maxiepishkin@gmail.com>
2024-06-09 12:44:05 +0100

    homework №8

---
+++ homework_08/constants.py
@@ -0,0 +1,26 @@
+from pywebio.input import NUMBER, TEXT
+
+MSG_WELCOME = 'Вітаємо на нашій вікторині.'
+MSG_USERNAME_REQUEST = 'Ваше імʼя'
+MSG_THANKS = 'Дякуємо за участь у нашій вікторині. Гарного вам дня!'
+MSG_RESULT = '<h2>Вітаю, {username}</h2>ваша оцінка становить <b>{score}</b> балла/баллів'
+
+QUESTION_1 = 'Коли почалась друга світова війна?'
+QUESTION_1_ANSWER = 1939
+QUESTION_1_TYPE = NUMBER
+
+QUESTION_2 = 'Через яку країну почалася 1 світова війна? (написати потрібно тільки назву країни)'
+QUESTION_2_ANSWER = 'Австріїська імперія'
+QUESTION_2_TYPE = TEXT
+
+QUESTION_3 = 'Коли Українська Народна Республіка (УНР) стала незалежною?'
+QUESTION_3_ANSWER = 1917
+QUESTION_3_TYPE = NUMBER
+
+QUESTION_4 = 'В честь кого назвали Напалеоновскі воїни? (писати поотрібно так: "В честь (імʼя правителя)"'
+QUESTION_4_ANSWER = 'В честь Напалеона Бонапарта'
+QUESTION_4_TYPE = TEXT
+
+QUESTION_5 = 'Хто відкрив шлях до Індії?'
+QUESTION_5_ANSWER = 'Васко да Гама'
+QUESTION_5_TYPE = TEXT
--- homework_08/homework.py
+++ homework_08/homework.py
@@ -1,95 +1,90 @@
 from pywebio.input import input as input_pw
-from pywebio.input import NUMBER
+from pywebio.input import NUMBER, TEXT
 from pywebio.output import put_text, put_html, put_image
 from pywebio import start_server
 from pywebio.session import run_js
 
-MSG_WELCOME = 'Вітаємо на нашій вікторині.'
-MSG_USERNAME = 'Ваше імʼя'
-MSG_QUESTION_1 = 'коли почалась друга світова війна?'
-MSG_QUESTION_2 = 'через яку країну почалася 1 світова війна? (написати потрібно тільки назву країни)'
-MSG_QUESTION_3 = 'коли Українська Народна Республіка (УНР) стала незалежною?'
-MSG_QUESTION_4 = 'в честь кого назвали Напалеоновскі воїни? (писати поотрібно так: "В честь (імʼя правителя)"'
-MSG_QUESTION_5 = 'хто відкрив шлях до Індії?'
-MSG_THANKS = 'Дякуємо за участь у нашій вікторині. Гарного вам дня!'
-total_score = 0
-question_1_answer = 1939
-question_2_answer = 'Австріїська імперія'
-question_3_answer = 1917
-question_4_answer = 'В честь Напалеона Бонапарта'
-question_5_answer = 'Васко да Гама'
-img_end = 'five_stars.jpeg'
-img_end_width = 200
+import constants
+import configs
 
 
-def convert_string_to_number(string_like_number: str) -> int:
-    if string_like_number.isdigit():
-        result = int(string_like_number)
-        return result
+def print_h1(message: str) -> None:
+    h1 = f'<h1>{message}</h1>'
+    put_html(h1)
 
 
-def get_formatted_html_h1(message: str) -> str:
-    result_h1 = f'<h1>{message}</h1>'
-    return result_h1
+def print_h2(message: str) -> None:
+    h2 = f'<h2>{message}</h2>'
+    put_html(h2)
 
 
-def get_formatted_html_h2(message: str) -> str:
-    result_h2 = f'<h2>{message}</h2>'
-    return result_h2
+def print_username_request() -> str:
+    username = input_pw(constants.MSG_USERNAME_REQUEST).strip().title()
+    return username
+
+
+def print_question(question: str, answer: str | int, type: str) -> bool:
+    users_answer = input_pw(question, type=type)
+
+    if type == TEXT:
+        answer = answer.lower()
+        users_answer = users_answer.lower()
+
+    result = users_answer == answer
+    return result
+
+
+def print_great_score() -> None:
+    img = open(configs.IMG_RESULT, 'rb').read()
+    put_image(img, width=f'{configs.IMG_RESULT_WIDTH}px')
+
+
+def print_final_score(username: str, score: int) -> None:
+    message = constants.MSG_RESULT.format(
+        username=username,
+        score=score
+    )
+    put_html(message)
+
+    if score == 5:
+        print_great_score()
+
+
+def print_thank_you() -> None:
+    print_h2(constants.MSG_THANKS)
+    timeout = str(configs.PAGE_RELOAD_TIMEOUT_MS)
+    run_js('setTimeout(function(){location.reload();}, ' + timeout + ' )')
 
 
 def main():
-    put_html(
-        get_formatted_html_h1(MSG_WELCOME)
-    )
-    username = input_pw(MSG_USERNAME)
-    question_1 = input_pw(f'{username}, {MSG_QUESTION_1}', type=NUMBER)
-    question_2 = input_pw(f'{username}, {MSG_QUESTION_2}')
-    question_3 = input_pw(f'{username}, {MSG_QUESTION_3}', type=NUMBER)
-    question_4 = input_pw(f'{username}, {MSG_QUESTION_4}')
-    question_5 = input_pw(f'{username}, {MSG_QUESTION_5}')
-    if question_1 == question_1_answer:
-        put_text('Правильно')
-        score_1 = 1
-    else:
-        put_text('Неправильно')
-        score_1 = 0
-    if question_2 == question_2_answer:
-        put_text('Правильно')
-        score_2 = 1
-    else:
-        put_text('Неправильно')
-        score_2 = 0
-    if question_3 == question_3_answer:
-        put_text('Правильно')
-        score_3 = 1
-    else:
-        put_text('Неправильно')
-        score_3 = 0
-    if question_4 == question_4_answer:
-        put_text('Правильно')
-        score_4 = 1
-    else:
-        put_text('Неправильно')
-        score_4 = 0
-    if question_5 == question_5_answer:
-        put_text('Правильно')
-        score_5 = 1
-    else:
-        put_text('Неправильно')
-        score_5 = 0
-    total_score = score_1 + score_2 + score_3 + score_4 + score_5
-    if total_score >= 0 and total_score < 5:
-        put_text(f'Кількість балів: {total_score}')
-    if total_score == 5:
-        put_text(f'Кількість балів: {total_score}')
-        img = open(img_end, 'rb').read()
-        put_image(img, width=f'{img_end_width}px')
-    put_html(
-        get_formatted_html_h2(MSG_THANKS)
-    )
-    run_js('setTimeout(function(){location.reload();}, 3000)')
+    print_h1(constants.MSG_WELCOME)
+
+    username = print_username_request()
+
+    total_score = 0
+    answer = print_question(constants.QUESTION_1, constants.QUESTION_1_ANSWER, constants.QUESTION_1_TYPE)
+    if answer:
+        total_score += 1
+
+    answer = print_question(constants.QUESTION_2, constants.QUESTION_2_ANSWER, constants.QUESTION_2_TYPE)
+    if answer:
+        total_score += 1
+
+    answer = print_question(constants.QUESTION_3, constants.QUESTION_3_ANSWER, constants.QUESTION_3_TYPE)
+    if answer:
+        total_score += 1
+
+    answer = print_question(constants.QUESTION_4, constants.QUESTION_4_ANSWER, constants.QUESTION_4_TYPE)
+    if answer:
+        total_score += 1
+
+    answer = print_question(constants.QUESTION_5, constants.QUESTION_5_ANSWER, constants.QUESTION_5_TYPE)
+    if answer:
+        total_score += 1
+
+    print_final_score(username, total_score)
+    print_thank_you()
 
 
 if __name__ == '__main__':
-    start_server(main, port=20000)
+    start_server(main, port=configs.SERVER_PORT)
