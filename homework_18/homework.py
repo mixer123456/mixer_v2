ebe9d424ac09102d21f7cd7d929a5c750014135a
max <maxiepishkin@gmail.com>
2024-07-17 20:05:39 +0100

    t

--- homework_18/homework.py
+++ homework_18/homework.py
@@ -1,9 +1,9 @@
 class Car:
 
-    def __init__(self, *, producer: str, brand: str, fuel_consumption: float, year_of_issue: int = 2020):
-        self.__year_of_issue = year_of_issue
-        self.producer = producer.title()
+    def __init__(self, *, brand: str, model: str, fuel_consumption: float, year: int = 2020):
+        self.__year_of_issue = year
         self.brand = brand.title()
+        self.model = model.title()
         self.__fuel_consumption = fuel_consumption
 
         self.__mileage = 0
@@ -16,7 +16,6 @@ class Car:
     @year_of_issue.setter
     def year_of_issue(self, value):
         self.__year_of_issue = value
-
 
     @property
     def mileage(self) -> int:
@@ -35,8 +34,16 @@ class Car:
         self.__fuel_consumption = value
 
     def __str__(self) -> str:
-        return f'This {self.__year_of_issue} car is from {self.producer} from {self.brand}'
+        if self.__fuel_consumption:
+            return f'{self.__year_of_issue} car from {self.brand} {self.model} model with fuel consumption {self.__fuel_consumption}'
+        else:
+            return f'{self.__year_of_issue} car from {self.brand} {self.model} model and it is electric car'
 
 
-car1 = Car(year_of_issue=2021, producer='ferdinand porsche', brand='porsche')
+car1 = Car(year=2021, brand='porsche', model='cayman', fuel_consumption=22.1)
 print(car1)
+car2 = Car(year=2023, brand='tesla', model='model x', fuel_consumption=0)
+print(car2)
+car3 = Car(year=2020, brand='mercedes', model='AMG', fuel_consumption=30.0)
+print(car3)
+
