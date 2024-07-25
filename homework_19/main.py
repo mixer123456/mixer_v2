f8ea181e33680f5a59cc8ef13ee38cf2377430b0
max <maxiepishkin@gmail.com>
2024-07-25 11:34:18 +0100

    19 homework

---
+++ homework_19/main.py
@@ -0,0 +1,33 @@
+from homework_19.library.Book import Book
+from homework_19.library.Magazine import Magazine
+from homework_19.library.DVD import DVD
+
+
+def main():
+    book1 = Book('The Hobbit', 'J.R.R. Tolkien', 1937, 320)
+    book2 = Book('The Lord of the Rings', 'J.R.R. Tolkien', 1954, 752)
+    book3 = Book('The Silmarillion', 'J.R.R. Tolkien', 1977, 365)
+
+    magazine1 = Magazine('Time', 'Time Inc.', 1926, 504)
+    magazine2 = Magazine('Time', 'Time Inc.', 1926, 90)
+    magazine3 = Magazine('Time', 'Time Inc.', 1926, 361)
+
+    dvd1 = DVD('The Lord of the Rings: The Fellowship of the Ring', 'Peter Jackson', 2001, 178)
+    dvd2 = DVD('The Lord of the Rings: The Two Towers', 'Peter Jackson', 2002, 179)
+    dvd3 = DVD('The Lord of the Rings: The Return of the King', 'Peter Jackson', 2003, 201)
+
+    print(book1.description())
+    print(book2.description())
+    print(book3.description())
+
+    print(magazine1.description())
+    print(magazine2.description())
+    print(magazine3.description())
+
+    print(dvd1.description())
+    print(dvd2.description())
+    print(dvd3.description())
+
+
+if __name__ == '__main__':
+    main()
