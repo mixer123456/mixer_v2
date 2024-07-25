fdaf85de90b53c07ad79d18b902b4c960a2834cb
max <maxiepishkin@gmail.com>
2024-07-25 09:46:09 +0100

    19 homework

--- homework_19/homework.py
+++ homework_19/homework.py
@@ -1,13 +1,4 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from random import randint
-
-
-@dataclass(frozen=True)
-class Params:
-    BOOK_NUMBER_OF_PAGES = randint(50, 201)
-    MAGAZINE_ISSUE_NUMBER = randint(1, 1001)
-    DVD_DURATION = randint(3, 6)
 
 
 class LibraryItem(ABC):
@@ -22,8 +13,8 @@ class LibraryItem(ABC):
 
 
 class Book(LibraryItem):
-    def __init__(self, title: str, author_or_director: str, year: int):
-        self.number_of_pages = Params.BOOK_NUMBER_OF_PAGES
+    def __init__(self, title: str, author_or_director: str, year: int, number_of_pages: int):
+        self.number_of_pages = number_of_pages
         super().__init__(title, author_or_director, year)
 
     def description(self):
@@ -31,8 +22,8 @@ class Book(LibraryItem):
 
 
 class Magazine(LibraryItem):
-    def __init__(self, title: str, author_or_director: str, year: int):
-        self.issue_number = Params.MAGAZINE_ISSUE_NUMBER
+    def __init__(self, title: str, author_or_director: str, year: int, issue_number: int):
+        self.issue_number = issue_number
         super().__init__(title, author_or_director, year)
 
     def description(self):
@@ -40,17 +31,40 @@ class Magazine(LibraryItem):
 
 
 class DVD(LibraryItem):
-    def __init__(self, title: str, author_or_director: str, year: int):
-        self.duration = Params.DVD_DURATION
+    def __init__(self, title: str, author_or_director: str, year: int, duration: int):
+        self.duration = duration
         super().__init__(title, author_or_director, year)
 
     def description(self):
         return f"DVD's title: {self.title},DVD's author/director: {self.author_or_director}, Year: {self.year}, Duration: {self.duration}"
 
 
-book = Book('Ivanhoe', 'Walter Scott', 1819)
-print(book.description())
-magazine = Magazine('Time', 'Henry Luce', 1923)
-print(magazine.description())
-dvd = DVD('Lord of The Ring', 'Peter Jackson', 2001)
-print(dvd.description())
+book1 = Book('The Hobbit', 'J.R.R. Tolkien', 1937, 320)
+book2 = Book('The Lord of the Rings', 'J.R.R. Tolkien', 1954, 752)
+book3 = Book('The Silmarillion', 'J.R.R. Tolkien', 1977, 365)
+
+magazine1 = Magazine('Time', 'Time Inc.', 1926, 504)
+magazine2 = Magazine('Time', 'Time Inc.', 1926, 90)
+magazine3 = Magazine('Time', 'Time Inc.', 1926, 361)
+
+dvd1 = DVD('The Lord of the Rings: The Fellowship of the Ring', 'Peter Jackson', 2001, 178)
+dvd2 = DVD('The Lord of the Rings: The Two Towers', 'Peter Jackson', 2002, 179)
+dvd3 = DVD('The Lord of the Rings: The Return of the King', 'Peter Jackson', 2003, 201)
+
+
+def main():
+    print(book1.description())
+    print(book2.description())
+    print(book3.description())
+
+    print(magazine1.description())
+    print(magazine2.description())
+    print(magazine3.description())
+
+    print(dvd1.description())
+    print(dvd2.description())
+    print(dvd3.description())
+
+
+if __name__ == '__main__':
+    main()
