2a1c1979050636900f10a4c627ed064ce12d5b2c
mixer123456 <maxiepishkin@gmail.com>
2024-08-12 09:28:34 +0100

    homework 21

--- homework_21/test_homework.py
+++ homework_21/test_homework.py
@@ -2,28 +2,20 @@ import pytest
 
 from homework import get_price_discounted
 
-testing_args_success = [
-    (80, 70, 10),
-    (50, 20, 30),
-    (60, 35, 25),
-]
 
+class TestGetPriceDiscounted:
+    def test_too_small_discount(self):
+        with pytest.raises(ValueError):
+            get_price_discounted(80, -70)
 
-@pytest.mark.parametrize('price, percent, expected_result', testing_args_success)
-def test_price_discounted_success(price: int | float, percent: int, expected_result: float):
-    actual_result = get_price_discounted(price, percent)
-    assert actual_result == expected_result
+    def test_too_big_discount(self):
+        with pytest.raises(ValueError):
+            get_price_discounted(80, 700)
 
+    def test_too_small_money(self):
+        with pytest.raises(ValueError):
+            get_price_discounted(-40454, 50)
 
-testing_args_fail = [
-    (80, '70', 10),
-    (-50, 20, 30),
-    (60, 350, 25),
-    (60, -40, 25),
-]
-
-
-@pytest.mark.parametrize('price, percent, expected_result', testing_args_fail)
-def test_price_discounted_fail(price: int | float, percent: int, expected_result: float):
-    actual_result = get_price_discounted(price, percent)
-    assert actual_result == expected_result
+    def test_str_instead_int_or_float(self):
+        with pytest.raises(TypeError):
+            get_price_discounted(40454, '50')
