a189ed591cab3299c96cff74e2cc10cde093d4c0
mixer123456 <maxiepishkin@gmail.com>
2024-08-20 13:09:39 +0100

    homework 23-24

--- homework_23-24/homework.py
+++ homework_23-24/homework.py
@@ -25,68 +25,68 @@ with sqlite3.connect(DB_PATH) as connection:
     """
     cursor.executescript(query)
 
-    # # add one school data
-    # address = 'Govorovo 3'
-    # floors = 2
-    # values = [address, floors]
-    # query = """
-    #         INSERT INTO city_schools(address, floors)
-    #         VALUES (?, ?)
-    # """
-    # cursor.execute(query, values)
-    #
-    # # add one student data
-    # surname = 'Ponchinskiy'
-    # name = 'Ponchik'
-    # specialization = ''
-    # school_id = 1
-    # values = [surname, name, specialization, school_id]
-    # query = """
-    #         INSERT INTO students(surname, name, specialization, school_id)
-    #         VALUES (?, ?, ?, ?)
-    # """
-    # cursor.execute(query, values)
-    #
-    # # add some school data
-    # values = [
-    #     ('Tenchitskaja 10', 3),
-    #     ('Pinchinskaja 6', 2),
-    #     ('Honskaja 9', 2),
-    # ]
-    # query = """
-    #         INSERT INTO city_schools(address, floors)
-    #         VALUES (?, ?)
-    # """
-    # cursor.executemany(query, values)
-    #
-    # # add some student data
-    # values = [
-    #     ('Nopnin', 'Ivan', '', 4),
-    #     ('Cinchik', 'Sasha', 'ecologist', 1),
-    #     ('Kofanija', 'Dasha', '', 3),
-    #     ('Nopnin', 'Ivan', '', 4),
-    #     ('Cinchik', 'Sasha', 'ecologist', 1),
-    #     ('Kofanija', 'Dasha', '', 3),
-    #     ('Nopnin', 'Ivan', '', 4),
-    #     ('Cinchik', 'Sasha', 'ecologist', 2),
-    #     ('Kofanija', 'Dasha', '', 2),
-    #     ('Nopnin', 'Ivan', '', 3),
-    #     ('Cinchik', 'Sasha', 'ecologist', 4),
-    #     ('Kofanija', 'Dasha', '', 1),
-    # ]
-    # query = """
-    #         INSERT INTO students(surname, name, specialization, school_id)
-    #         VALUES (?, ?, ?, ?)
-    # """
-    # cursor.executemany(query, values)
+    # add one school data
+    address = 'Govorovo 3'
+    floors = 2
+    values = [address, floors]
+    query = """
+            INSERT INTO city_schools(address, floors)
+            VALUES (?, ?)
+    """
+    cursor.execute(query, values)
+
+    # add one student data
+    surname = 'Ponchinskiy'
+    name = 'Ponchik'
+    specialization = ''
+    school_id = 1
+    values = [surname, name, specialization, school_id]
+    query = """
+            INSERT INTO students(surname, name, specialization, school_id)
+            VALUES (?, ?, ?, ?)
+    """
+    cursor.execute(query, values)
+
+    # add some school data
+    values = [
+        ('Tenchitskaja 10', 3),
+        ('Pinchinskaja 6', 2),
+        ('Honskaja 9', 2),
+    ]
+    query = """
+            INSERT INTO city_schools(address, floors)
+            VALUES (?, ?)
+    """
+    cursor.executemany(query, values)
+
+    # add some student data
+    values = [
+        ('Nopnin', 'Ivan', '', 4),
+        ('Cinchik', 'Sasha', 'ecologist', 1),
+        ('Kofanija', 'Dasha', '', 3),
+        ('Nopnin', 'Ivan', '', 4),
+        ('Cinchik', 'Sasha', 'ecologist', 1),
+        ('Kofanija', 'Dasha', '', 3),
+        ('Nopnin', 'Ivan', '', 4),
+        ('Cinchik', 'Sasha', 'ecologist', 2),
+        ('Kofanija', 'Dasha', '', 2),
+        ('Nopnin', 'Ivan', '', 3),
+        ('Cinchik', 'Sasha', 'ecologist', 4),
+        ('Kofanija', 'Dasha', '', 1),
+    ]
+    query = """
+            INSERT INTO students(surname, name, specialization, school_id)
+            VALUES (?, ?, ?, ?)
+    """
+    cursor.executemany(query, values)
 
     # read data
-    # query = """
-    #     SELECT surname, name, specialization, school_id
-    #     FROM students
-    # """
-    # result = cursor.execute(query)
-    # pprint(result.fetchall(), width=70)
+    query = """
+        SELECT surname, name, specialization, school_id
+        FROM students
+    """
+    result = cursor.execute(query)
+    pprint(result.fetchall(), width=70)
 
     111
     # homework 24
